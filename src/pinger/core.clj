b7624d4a5eaec6706af63f59ce685346ad2d1d62
apostaat <54319739+apostaat@users.noreply.github.com>
2020-04-16 01:19:52 +0300

    Add files via upload

---
+++ src/pinger/core.clj
@@ -0,0 +1,29 @@
+(ns pinger.core
+  (:gen-class))
+
+(defn timed-ping
+      "Time an .isReachable ping to a given domain"
+      [domain timeout]
+      (let [addr (java.net.InetAddress/getByName domain)
+            start (. System (nanoTime))
+            result (.isReachable addr timeout)
+            total (/ (double (- (. System (nanoTime)) start)) 1000000.0)]
+           {:time total
+            :result result}))
+
+(defn try-catch-timed-ping
+  "Try/catch and get string of result for timed ping"
+  []
+  (try (str (timed-ping "ya.ru" 5000))
+       (catch Exception e (str "caught exception: " (.getMessage e)))))
+
+(def log "/Users/artemalekseev/Downloads/lein projects/pinger/resources/log.txt")
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+      (loop [attempt 1 ]
+           (do
+             (spit log (str  attempt " "  (try-catch-timed-ping) "\n") :append true )
+             (println (str "I ping to ya.ru, this is my: " attempt " attempt." ))
+             (recur (inc attempt)))))
